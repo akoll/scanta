964d91360a61a7b51a6ff31fdf0d4c32b2f16439
AKoll <31693992+AKoll@users.noreply.github.com>
2020-06-07 14:02:36 +0200

    Wrap entity parameters in reference_wrapper

--- runtime/sequential.hpp
+++ runtime/sequential.hpp
@@ -37,7 +37,6 @@ public:
     hana::for_each(_systems, [this](auto& system) {
       constexpr auto argtypes = hana::transform(argtypes_of<decltype(system)>, hana::traits::decay);
       constexpr auto component_argtypes = hana::intersection(hana::to_set(argtypes), component_types);
-
       (typename decltype(hana::unpack(component_argtypes, hana::template_<ForEntitiesWith>))::type){}(
         _storage, [&](Entity entity) {
           auto args = hana::transform(argtypes, [this, &entity](auto argtype) {
@@ -52,11 +51,10 @@ public:
               [&](auto _) { return hana::eval_if(
                 // Check if the argument type is a stored system type:
                 hana::find(system_types, argtype) != hana::nothing,
-                // reference_wrapper is needed to store the reference in the args container (to later be unpacked into the system call).
                 [&](auto _) { return std::reference_wrapper(std::get<ArgType>(_(_systems))); },
                 [&](auto _) { return hana::eval_if(
                   argtype == hana::type_c<Entity>,
-                  [&](auto _) { return entity; },
+                  [&](auto _) { return std::reference_wrapper(entity); },
                   [&](auto _) { static_assert(_(false), "System argument types are invalid"); }
                 ); }
               ); }
@@ -65,7 +63,6 @@ public:
           return hana::unpack(args, system);
         }
       );
-
     });
   }
 
@@ -73,6 +70,7 @@ private:
   // System types in decayed form (removes cv-qualifiers and reference).
   static constexpr auto system_types = hana::transform(hana::tuple_t<TSystems...>, hana::traits::decay);
   // Set of component types used by any system in decayed form (removes cv-qualifiers and reference).
+  // System-types and special types (Entity) are discarded.
   static constexpr auto component_types = hana::difference(
     hana::to_set(hana::transform(
       hana::flatten(
