9d726e67ad809a742590aa603376274c26c5b046
AKoll <31693992+AKoll@users.noreply.github.com>
2020-06-07 10:29:53 +0200

    Add bitset2 and use constexpr entity signatures

--- storage/tuple_of_vecs.hpp
+++ storage/tuple_of_vecs.hpp
@@ -1,36 +1,71 @@
 #pragma once
 
+#include <iostream>
 #include <tuple>
 #include <vector>
+
+#include "../include/bitset2/bitset2.hpp"
+
+#include <boost/hana.hpp>
+using namespace hana::literals;
 
 namespace ecs {
   namespace storage {
 
   template<typename... TComponents>  
   class TupleOfVecs {
+  private:
+    static constexpr auto _component_types = hana::tuple_t<TComponents...>;
+    using Signature = Bitset2::bitset2<sizeof...(TComponents)>;
+
+    template<typename TComponent>
+    static constexpr size_t _component_index = hana::find_if(hana::make_range(0_c, hana::size_c<sizeof...(TComponents)>), [](auto index) { return _component_types[index] == hana::type_c<TComponent>; }).value();
+
+    template<typename TFirst>
+    static constexpr auto make_signature() {
+      return Signature(1) << _component_index<TFirst>;
+    }
+
+    template<typename TFirst, typename TSecond, typename... TRest>
+    static constexpr auto make_signature() {
+      return make_signature<TSecond, TRest...>() | (Signature(1) << _component_index<TFirst>);
+    }
+
+    template<typename... TRequiredComponents>
+    static constexpr Signature signature_of = make_signature<TRequiredComponents...>();
   public:
     using Entity = size_t;
+    struct EntityMetadata {
+       Signature signature;
+    };
 
     // TODO: REMOVE
     TupleOfVecs() {
-      [](auto...){}((std::get<std::vector<TComponents>>(_vectors).resize(2), 0)...);
+      _entities.resize(2);
+      for (EntityMetadata& entity_metadata : _entities) entity_metadata.signature = signature_of<TComponents...>;
+      [](auto...){}((std::get<std::vector<TComponents>>(_components).resize(2), 0)...);
       size = 2;
     }
 
     template<typename TComponent>
     TComponent& get_component(Entity entity) {
-      return std::get<std::vector<TComponent>>(_vectors)[entity];
+      return std::get<std::vector<TComponent>>(_components)[entity];
     }
 
     template<typename... TRequiredComponents>
     void for_entities_with(auto callable) {
-      for (size_t i{0}; i < size; ++i)
-        callable(Entity{i});
+      constexpr Signature signature = signature_of<TRequiredComponents...>;
+      std::cout << signature << std::endl;
+      for (size_t i{0}; i < size; ++i) {
+        if ((_entities[i].signature & signature) == signature)
+          callable(Entity{i});
+      }
     }
 
   private:
     size_t size;
-    std::tuple<std::vector<TComponents>...> _vectors;
+    std::vector<EntityMetadata> _entities;
+    std::tuple<std::vector<TComponents>...> _components;
   };
 
   }
