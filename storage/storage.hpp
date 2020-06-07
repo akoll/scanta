9dbd3ae87644a692cc1f719e3dd1c92b26126529
AKoll <31693992+AKoll@users.noreply.github.com>
2020-06-07 14:19:07 +0200

    Fix storage concept to use std::convertible_to

--- storage/storage.hpp
+++ storage/storage.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
 #include <stddef.h>
-#include <type_traits>
+#include <concepts>
 
 template<template<typename...> typename TStorage>
-concept Storage = /* requires(TStorage<size_t> storage) {
-   { storage.template get_component<size_t>(0) } -> size_t&;
-} && */ !std::is_empty_v<typename TStorage<>::Entity>;
+concept Storage = requires(TStorage<size_t> storage) {
+   { storage.template get_component<size_t>(0) } -> std::convertible_to<size_t&>;
+} && !std::is_empty_v<typename TStorage<>::Entity>;
