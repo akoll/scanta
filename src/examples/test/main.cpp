eadbd7acf1e8ce60d7081df78b5308dca7d03fab
AKoll <31693992+AKoll@users.noreply.github.com>
2020-07-26 18:57:39 +0200

    Change example from sine to spiral

--- src/examples/test/main.cpp
+++ src/examples/test/main.cpp
@@ -20,41 +20,16 @@ struct Age {
   float seconds_live;
 };
 
-struct Lifetime {
-  float seconds_left;
-};
-
-
-class MoveRightSystem {
-public:
-  void operator()(ECS::Entity entity, float delta_time, Transform& transform, const Age& age) {
-    transform.pos.x -= delta_time * (200 + glm::sin(age.seconds_live * 10) * 100);
-    transform.pos.y += delta_time * glm::sin(age.seconds_live * 3) * 50;
-  }
-};
-
-class RenderSystem {
-public:
-  RenderSystem(SDL_Surface* surface) : _surface(surface) {}
-
-  void operator()(ECS::Entity entity, const Transform& transform, const Age& age, const Lifetime& lifetime) {
-    SDL_Rect rect{int(transform.pos.x - 4), int(transform.pos.y - 4), 8, 8};
-    Uint8 col = std::max(lifetime.seconds_left / 3 * 0xff, 0.0f);
-    SDL_FillRect(_surface, &rect, SDL_MapRGB(_surface->format,
-      (std::sin(age.seconds_live) + 1.0) * 0.5 * col,
-      (std::cos(age.seconds_live / 30.0) + 1.0) * 0.5 * col,
-      (std::sin(age.seconds_live / 30.0) + 1.0) * 0.5 * col
-    ));
-  }
-private:
-  SDL_Surface* _surface;
-};
-
 class FrametimeSystem {
 public:
   FrametimeSystem() : _last(std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now()).time_since_epoch().count()) {}
 
+  float get_seconds_total() const {
+    return _seconds_total;
+  }
+
   void operator()(const ecs::RuntimeManager& manager, double delta_time) {
+    _seconds_total += delta_time;
     long long now = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now()).time_since_epoch().count();
     if (now - _last >= 500) {
       std::cout << manager.get_entity_count() << "#, " << delta_time * 1000 << "ms" << std::endl;
@@ -63,6 +38,33 @@ public:
   }
 private:
   long long _last;
+  float _seconds_total = 0.0f;
+};
+
+class MoveRightSystem {
+public:
+  void operator()(const FrametimeSystem& frametime_system, ECS::Entity entity, float delta_time, Transform& transform, const Age& age) {
+    transform.pos = glm::vec2{
+      320 - glm::sin(age.seconds_live * 2 - frametime_system.get_seconds_total()) * age.seconds_live * 45 / (1.0f + frametime_system.get_seconds_total() / 5),
+      240 + glm::cos(age.seconds_live * 2 - frametime_system.get_seconds_total()) * age.seconds_live * 45 / (1.0f + frametime_system.get_seconds_total() / 5)
+    };
+  }
+};
+
+class RenderSystem {
+public:
+  RenderSystem(SDL_Surface* surface) : _surface(surface) {}
+
+  void operator()(ECS::Entity entity, const Transform& transform, const Age& age) {
+    SDL_Rect rect{int(transform.pos.x - 1), int(transform.pos.y - 1), 2, 2};
+    SDL_FillRect(_surface, &rect, SDL_MapRGB(_surface->format,
+      (std::sin(age.seconds_live / 20.0) + 1.0) * 0.5 * 0xff,
+      (std::cos(age.seconds_live / 30.0) + 1.0) * 0.5 * 0xff,
+      (std::sin(age.seconds_live / 30.0) + 1.0) * 0.5 * 0xff
+    ));
+  }
+private:
+  SDL_Surface* _surface;
 };
 
 class SpawnSystem {
@@ -76,23 +78,18 @@ public:
       _last = now;
       spawn = true;
     }
-    return [spawn, now](const auto& manager) {
-      if (spawn) manager.new_entity(Transform{{640, 240 - glm::sin(now * 0.002) * 170}}, Lifetime{3}, Age{0});
+    return [spawn](const auto& manager) {
+      if (spawn) manager.new_entity(Transform{}, Age{0});
     };
   }
 private:
   long long _last;
 };
 
-class LifetimeSystem {
+class AgeSystem {
 public:
-  auto operator()(ECS::Entity entity, Age& age, Lifetime& lifetime, float delta_time) {
-    lifetime.seconds_left -= delta_time;
+  auto operator()(Age& age, float delta_time) {
     age.seconds_live += delta_time;
-    bool kill = lifetime.seconds_left <= 0;
-    return [entity, kill](const auto& manager) {
-      if (kill) manager.remove_entity(entity);
-    };
   }
 };
 
@@ -100,7 +97,7 @@ int main() {
   SDL_Window* window = NULL;
   SDL_Surface* screen_surface = nullptr;
 
-  if (SDL_Init( SDL_INIT_VIDEO ) < 0) {
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
     return 1;
   }
@@ -113,10 +110,10 @@ int main() {
 
   MoveRightSystem move_sys;
   ECS::Runtime tick(
-    std::move(move_sys),
     FrametimeSystem{},
+    std::move(move_sys),
     SpawnSystem{},
-    LifetimeSystem{},
+    AgeSystem{},
     RenderSystem(screen_surface)
   );
 
