4732a24b17c39fc618937d5392cbc80459e3cdf9
AKoll <31693992+AKoll@users.noreply.github.com>
2020-07-26 17:31:57 +0200

    Remove empty line

--- src/ecs/runtime/sequential.hpp
+++ src/ecs/runtime/sequential.hpp
@@ -65,7 +65,6 @@ public:
                     [&](auto _) { static_assert(_(false), "System argument types are invalid"); }
                   );
                 }
-
               }
             }
           }
