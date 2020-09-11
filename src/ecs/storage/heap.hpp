ea994cac46872b296d3dafe1fb08713e4b396999
AKoll <31693992+AKoll@users.noreply.github.com>
2020-09-11 16:41:12 +0200

    Merge the two heap implementations into one with a compile-time switch

--- src/ecs/storage/heap.hpp
+++ src/ecs/storage/heap.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <tuple>
 #include <vector>
+#include <memory>
 #include <type_traits>
 
 
@@ -13,54 +14,64 @@ using namespace hana::literals;
 namespace ecs::storage {
 
 // Base declaration for partial specialization.
-template<typename... TStoredComponents>
+template<bool smart, typename... TStoredComponents>
 class Heap;
 
 // Partial specialization for the case of no stored components.
 // This is required because an entity handle type needs to be exposed
 // to the scaffold before the list of stored components is known.
-template<>
-class Heap<> {
+template<bool smart>
+class Heap<smart> {
 public:
   class Entity {
-    template<typename...>
+    template<bool, typename...>
     friend class Heap;
   private:
-    void* _pointer;
+    using VoidPointer = typename std::conditional<smart, std::shared_ptr<void>, void*>::type;
+    VoidPointer _pointer;
   public:
-    Entity(void* pointer) : _pointer(pointer) {}
+    Entity(VoidPointer pointer) : _pointer(pointer) {}
   };
 };
 
-template<typename... TStoredComponents>
-class Heap : Heap<> {
+template<bool smart, typename... TStoredComponents>
+class Heap : Heap<smart> {
 private:
   static constexpr auto _component_types = hana::tuple_t<TStoredComponents...>;
+
+  template<typename T>
+  using Pointer = typename std::conditional<smart, std::shared_ptr<T>, T*>::type;
 public:
   struct EntityData {
-    std::tuple<TStoredComponents*...> components;
+    std::tuple<Pointer<TStoredComponents>...> components;
   };
   // using Entity = EntityData*;
 
   class Entity {
   private:
-    EntityData* _pointer;
+    Pointer<EntityData> _pointer;
   public:
-    Entity(EntityData* pointer) : _pointer(pointer) {}
+    Entity(Pointer<EntityData> pointer) : _pointer(pointer) {}
 
-    Entity(Heap<>::Entity other) {
-      _pointer = static_cast<EntityData*>(other._pointer);
+    Entity(typename Heap<smart>::Entity other) {
+      if constexpr (smart)
+        _pointer = std::static_pointer_cast<EntityData>(other._pointer);
+      else
+        _pointer = static_cast<EntityData*>(other._pointer);
     }
 
-    operator Heap<>::Entity() {
-      return static_cast<Heap<>::Entity>(_pointer);
+    operator typename Heap<smart>::Entity() {
+      if constexpr (smart)
+        return std::static_pointer_cast<Heap<smart>::Entity>(_pointer);
+      else
+        return static_cast<typename Heap<smart>::Entity>(_pointer);
     }
 
-    operator EntityData*() {
+    operator Pointer<EntityData>() {
       return _pointer;
     }
 
-    EntityData* operator->() {
+    Pointer<EntityData> operator->() {
       return _pointer;
     }
   };
@@ -68,7 +79,7 @@ public:
   template<typename TComponent>
   TComponent& get_component(Entity entity) {
     // TODO: static_assert component type handled
-    return *std::get<TComponent*>(entity->components);
+    return *std::get<Pointer<TComponent>>(entity->components);
   }
 
   size_t get_size() {
@@ -79,20 +90,27 @@ public:
   void set_components(Entity entity, TComponents&&... components) {
     // TODO: static_assert component type handled
     // Copy components from parameters.
-    ((std::get<std::decay_t<TComponents>*>(entity->components) = new TComponents(std::forward<TComponents>(components))), ...);
+    if constexpr (smart)
+      ((std::get<std::shared_ptr<std::decay_t<TComponents>>>(entity->components) = std::make_shared<TComponents>(std::forward<TComponents>(components))), ...);
+    else
+      ((std::get<std::decay_t<TComponents>*>(entity->components) = new TComponents(std::forward<TComponents>(components))), ...);
   }
 
   // TODO: return entity?
   void new_entity(auto&&... components) {
-    EntityData* entity_data = new EntityData();
+    Pointer<EntityData> entity_data;
+    if constexpr (smart)
+      entity_data = std::make_shared<EntityData>();
+    else
+      entity_data = new EntityData();
     _entities.push_back(entity_data);
     set_components(entity_data, std::forward<decltype(components)>(components)...);
   }
 
   void remove_entity(Entity entity) {
-    auto it = std::find(_entities.begin(), _entities.end(), static_cast<EntityData*>(entity));
+    auto it = std::find(_entities.begin(), _entities.end(), static_cast<Pointer<EntityData>>(entity));
     if (it != _entities.end()) {
-      delete *it;
+      if constexpr (!smart) delete *it;
       _entities.erase(it);
     }
     // else: entity not found
@@ -102,17 +120,23 @@ public:
   void for_entities_with(auto callable) {
     if constexpr(sizeof...(TRequiredComponents) > 0) {
       // TODO: static_assert component types handled
-      for (EntityData* entity_data : _entities) {
-        if ((... && std::get<TRequiredComponents*>(entity_data->components)))
-          callable(entity_data);
+      for (Pointer<EntityData> entity_data : _entities) {
+        if ((... && std::get<Pointer<TRequiredComponents>>(entity_data->components)))
+          callable(static_cast<typename Heap<smart>::Entity>(entity_data));
       }
-    } else callable(nullptr); // TODO: move check to runtime to avoid 0-reservation
+    } else callable(typename Heap<smart>::Entity(nullptr)); // TODO: move check to runtime to avoid 0-reservation
   }
 
   // TODO: for_entities_with_parallel
 
 private:
-  std::vector<EntityData*> _entities;
+  std::vector<Pointer<EntityData>> _entities;
 };
+
+template<typename... TComponents>
+class ExplicitHeap : public Heap<false, TComponents...> {};
+
+template<typename... TComponents>
+class SmartHeap : public Heap<true, TComponents...> {};
 
 }
