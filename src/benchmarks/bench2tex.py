c8fa28d6854b408b44eb3aa2a8bda46079db9d8d
AKoll <31693992+AKoll@users.noreply.github.com>
2020-09-11 18:50:47 +0200

    Add offset parameter to bench2tex.py

--- src/benchmarks/bench2tex.py
+++ src/benchmarks/bench2tex.py
@@ -5,11 +5,12 @@ import sys
 print(r'\addplot[thin,%s] coordinates {' % sys.argv[1])
 
 multiplier = int(sys.argv[2]) if len(sys.argv) > 2 else 1
+offset = int(sys.argv[3]) if len(sys.argv) > 3 else 0
 
 index = 0
 for line in sys.stdin:
   if index >= 0:
-    print('  ({}, {})'.format(index * multiplier, line.rstrip()))
+    print('  ({}, {})'.format(offset + index * multiplier, line.rstrip()))
   index += 1
 
 print('};')
