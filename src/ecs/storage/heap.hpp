49314e546cb46745e01e27f84ad3195e6b4e6fe8
AKoll <31693992+AKoll@users.noreply.github.com>
2020-09-11 17:58:50 +0200

    Document heap storage

--- src/ecs/storage/heap.hpp
+++ src/ecs/storage/heap.hpp
@@ -67,13 +67,27 @@ private:
   template<typename T>
   using Pointer = typename std::conditional<smart, std::shared_ptr<T>, T*>::type;
 public:
-  /// Entity metadata.
-  ///
-  /// The associated components are referenced in a tuple of pointers.
-  /// For each component type, a pointer is stored, pointing to the component data.
-  /// When a component is not enabled for the entity, the pointer is null.
+  /// Entity metadata used by the storage internally.
   struct EntityData {
+    /// The associated components are referenced in a tuple of pointers.
+    /// For each component type, a pointer is stored, pointing to the component data.
+    /// When a component is not enabled for the entity, the pointer is null.
     std::tuple<Pointer<TStoredComponents>...> components;
+
+    /// Deletes all components associated with this entity.
+    void clear_components() {
+      // If regular pointers are used, memory needs to be freed first using `delete`.
+      if constexpr (!smart)
+        ((delete std::get<Pointer<std::decay_t<TStoredComponents>>>(components)), ...);
+      // Set all pointer to null using a fold-expression.
+      ((std::get<Pointer<std::decay_t<TStoredComponents>>>(components) = nullptr), ...);
+    }
+
+    /// Destructor which deletes all components.
+    ~EntityData() {
+      if constexpr (!smart)
+        ((delete std::get<Pointer<std::decay_t<TStoredComponents>>>(components)), ...);
+    }
   };
 
   /// The entity handle type of the heap storage.
@@ -138,15 +152,21 @@ public:
     return _entities.size();
   }
 
+  // TODO: Add set_component for single components?
+
   /// Sets the component data for some entity.
   ///
-  /// The components are passed in as temporary rvalue-references. For each one, memory is then allocated
+  /// Any components previously enabled on the entity and not passed in again are disabled.
+  /// The passed in components make up the new complete set of components for that entity.
+  /// Components are passed in as temporary rvalue-references. For each one, memory is then allocated
   /// and the component temporary is copied into the heap. The resulting pointer is stored in the entity metadata tuple.
   /// @param entity The entity for which to set the components.
-  /// @params component The component data rvalues to be assigned.
+  /// @params components The component data rvalues to be assigned.
   template<typename... TComponents>
   void set_components(Entity entity, TComponents&&... components) {
     // TODO: static_assert component type handled
+    // Remove all components from the entity.
+    entity->clear_components();
     // Copy components from parameters.
     if constexpr (smart)
       // The component rvalue parameter-pack is unpacked using a fold-expression.
@@ -164,46 +184,77 @@ public:
       );
   }
 
+  // TODO: Add remove_component?
+
   // TODO: return entity?
+  /// Create a new entity.
+  ///
+  /// Allocates new memory for the entity metadata.
+  /// @param components The set of components to be initially associated with the new entity.
   void new_entity(auto&&... components) {
+    // Pointer to the new entity metadata.
     Pointer<EntityData> entity_data;
+    // Construct either shared or plain.
     if constexpr (smart)
       entity_data = std::make_shared<EntityData>();
     else
       entity_data = new EntityData();
+    // Add the new metadata pointer to the entity vector.
     _entities.push_back(entity_data);
+    // Set initial components by forwarding them (retaining references without copy).
     set_components(entity_data, std::forward<decltype(components)>(components)...);
   }
 
+  /// Removes an entity from the storage.
+  ///
+  /// Frees any memory for the entity metadata and associated components.
   void remove_entity(Entity entity) {
+    // Find the entity in the vector of pointers. This is O(N).
     auto it = std::find(_entities.begin(), _entities.end(), static_cast<Pointer<EntityData>>(entity));
+    // If the entity has been found (/ is stored).
     if (it != _entities.end()) {
+      // Delete it. This also deletes all components.
       if constexpr (!smart) delete *it;
+      // Remove it from the entity vector.
       _entities.erase(it);
     }
-    // else: entity not found
+    // TODO: else: entity not found
   }
 
+  /// Execute a callable on each entity with all required components enabled.
+  ///
+  /// @tparam TRequiredComponents The set of component types required to be enabled for an entity to be processed.
+  /// @param callable The callable to be executed with each matched entity's index as an argument.
   template<typename... TRequiredComponents>
   void for_entities_with(auto callable) {
+    /// If the list of required component types is empty, the callable is called exactly once.
     if constexpr(sizeof...(TRequiredComponents) > 0) {
       // TODO: static_assert component types handled
+      // Iterate all stored entities.
       for (Pointer<EntityData> entity_data : _entities) {
+        // Check the entity signature by seeing if all required component pointers are non-null.
+        // This is done using a fold-expression with the boolean AND operator. Since any non-null pointer
+        // is truthy and null-pointers are falsey, this is equivalent to a signature match.
         if ((... && std::get<Pointer<TRequiredComponents>>(entity_data->components)))
+          // Cast the entity handle to the base handle type for systems to process them.
           callable(static_cast<typename Heap<smart>::Entity>(entity_data));
       }
+      // Single-fire systems get a null-pointer as the entity handle.
     } else callable(typename Heap<smart>::Entity(nullptr)); // TODO: move check to runtime to avoid 0-reservation
   }
 
   // TODO: for_entities_with_parallel
 
 private:
+  /// Vector storing the entity metadata pointers.
   std::vector<Pointer<EntityData>> _entities;
 };
 
+// Convenience type alias of non-smart storage for passing the type into the scaffold ECS class.
 template<typename... TComponents>
 class ExplicitHeap : public Heap<false, TComponents...> {};
 
+// Convenience type alias of smart storage for passing the type into the scaffold ECS class.
 template<typename... TComponents>
 class SmartHeap : public Heap<true, TComponents...> {};
 
