401ec66d2ef8c0722dccdff950194db0e348ff33
AKoll <31693992+AKoll@users.noreply.github.com>
2020-10-28 12:29:27 +0100

    Add a convenience header for including the entire ecs library

---
+++ src/ecs/ecs.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "scaffold/ecs.hpp"
+#include "runtime/sequential.hpp"
+#include "runtime/parallel.hpp"
+#include "storage/scattered.hpp"
+#include "storage/vector_of_tuples.hpp"
+#include "storage/tuple_of_vectors.hpp"
