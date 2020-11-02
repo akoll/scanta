Fix averaging output in bench2avg.py

src/benchmarks/s2bench/bench2avg.py
@@ -10,10 +10,9 @@ repetitions = int(sys.argv[1]) if len(sys.argv) > 1 else 1
 values = np.array([float(line.rstrip()) for line in sys.stdin])
 values = values.reshape(repetitions, values.shape[0] // repetitions)
 
-averages = np.average(values, axis=0)
-
-if repetitions == 1:
-  print(averages)
-else:
+if repetitions > 1:
+  averages = np.average(values, axis=0)
   for avg in averages:
     print(avg)
+else:
+  print(np.average(values))